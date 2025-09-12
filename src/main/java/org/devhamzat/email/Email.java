7bf71a74d70ea1e52700978f0956ac5024ce63c0
devhamzat <ayomidelegbede@gmail.com>
2025-09-12 08:12:46 +0100

    constraynt is back

--- src/main/java/org/devhamzat/email/Email.java
+++ src/main/java/org/devhamzat/email/Email.java
@@ -12,7 +12,7 @@ import static java.util.regex.Pattern.CASE_INSENSITIVE;
 public class Email implements ConstraintValidator<ValidateEmail, CharSequence> {
     private static final int MAX_LOCAL_PART_LENGTH = 64;
     private static final String LOCAL_PART_ATOM = "[a-z0-9!#$%&'*+/=?^_`{|}~\u0080-\uFFFF-]";
-    private static final String LOCAL_PART_INSIDE_QUOTES_ATOM = "(?:[a-z0-9!#$%&'*.(),<>\\[\\]:;  @+/=?^_`{|}~\u0080-\uFFFF-]|\\\\\\\\|\\\\\\\")";
+    private static final String LOCAL_PART_INSIDE_QUOTES_ATOM = "[a-z0-9!#$%&'*.(),<>\\[\\]:;  @+/=?^_`{|}~\u0080-\uFFFF-]|\\\\\\\\|\\\\\"";
 
     private static final Pattern LOCAL_PART_PATTERN = Pattern.compile(
             "(?:" + LOCAL_PART_ATOM + "+|\"" + LOCAL_PART_INSIDE_QUOTES_ATOM + "+\")" +
